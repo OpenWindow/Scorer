8cb46ff9e5b24a4dc448aa97a68e1dea72c78ac7
Neel Mallepally <neelavardhanreddym@gmail.com>
2014-03-19 21:18:01 -0400

    Initial Project Creation

---
+++ Scorer.Mobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scorer.Mobile.Startup))]
+namespace Scorer.Mobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
